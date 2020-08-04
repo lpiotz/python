810f1dec4ac4751de2023d85b066a502af5052d4
Lohaina Piotz <67489593+lpiotz@users.noreply.github.com>
2020-08-03 23:20:55 -0300

    Add files via upload

---
+++ CreatingFunction1.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("Hello, Friends!")
+
+say_hello()
+say_hello()
+say_hello()
---
+++ CreatingFunction2.py
@@ -0,0 +1,12 @@
+def car_wash(amount_paid):
+    if (amount_paid == 12):
+        print("Wash with tri-color foam")
+        print("Rinse later")
+        print("Dry with large blow dryper")
+
+    if (amount_paid == 6):
+        print("Wash with white foam")
+        print("Rinse Once")
+        print("air dry")
+
+car_wash(6)
